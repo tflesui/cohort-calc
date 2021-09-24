4e47570cf68d187d13fe7a6a64cfbbafdb5b1fdb
Francis Lesui <37851797+tflesui@users.noreply.github.com>
2021-09-23 19:24:44 -0700

    Added bootstrap for responsiveness

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import { Button, Container } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container style={{ marginTop: 20 }}>
+        <Button color="success">Lessgo!</Button>
+      </Container>
     </div>
   );
 }
